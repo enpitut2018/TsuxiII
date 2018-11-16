2bc535e3f1dd3b208ba75bd773f08266b45e2354
takuya hashimoto <t.hashimo.s@gmail.com>
2018-11-16 16:46:34 +0900

    unchi

--- app/controllers/destination_form_controller.rb
+++ app/controllers/destination_form_controller.rb
@@ -12,12 +12,17 @@ class DestinationFormController < ApplicationController
     destination_2 = params[:destination_2]
     @hour = params[:hour]
     @minute = params[:minute]
+    
+    @c_hour = params[:c_hour]
+    @c_minute = params[:c_minute]
+
+
     @keiyu_array = [destination_1, destination_2] # 経由地
     @sk_res = []                                  # 出発地から経由地のjson(s:start, k:keiyu)
     kk_res = []                                   # 経由地間のjson
     sk_keisan =[]                                 # 出発地から経由地の途中計算
 
-    # スタートから経由地の時間を取得する
+    # スタートからそれぞれの経由地の時間を取得する
     [destination_1, destination_2].each do |d|
       uri = URI.encode('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='+@origin+'&destinations='+d+'&mode=driving&key='+ENV['API_KEY'])
       json = Net::HTTP.get(URI.parse(uri))
@@ -87,43 +92,123 @@ class DestinationFormController < ApplicationController
       @sk_hour -= 24
     end
 
-    # 0-9分以内の場合の分表示の設定(by t:1109)
-    if @sk_minute < 10
-      @sk_minute = "0" + @sk_minute.to_s
-    end
+    # # 0-9分以内の場合の分表示の設定(by t:1109)
+    # if @sk_minute < 10
+    #   @sk_minute = "0" + @sk_minute.to_s
+    # end
 
 
-    # k(経由地1)からk(経由地2)までの時間演算
+    
     
     # via→goalの時間の初期設定(by t:1109)
-    @kk_h = @sk_hour.to_i
-    @kk_m = @sk_minute.to_i
+    @skk_h = @sk_hour.to_i
+    @skk_m = @sk_minute.to_i
 
     if @kk_time =~ /\shours|\shour/
-      @kk_h = @kk_h + $`.to_i
+      @skk_h = @skk_h + $`.to_i
       if @kk_time =~ /\shours\s(.+)\smins|\shours\s(.+)\smin|\shour\s(.+)\smins|\shour\s(.+)\smin/
-        @kk_m = @kk_m + $+.to_i
+        @skk_m = @skk_m + $+.to_i
       end
     elsif @kk_time =~ /\smins|\smin/
-      @kk_m = $`.to_i + @kk_m
+      @skk_m = $`.to_i + @skk_m
     end 
 
     # 分が60を越える時の設定(by t:1109)
-    if @kk_m >= 60
-      @kk_h += 1
-      @kk_m -= 60
+    if @skk_m >= 60
+      @skk_h += 1
+      @skk_m -= 60
     end
 
     # 時間が24時を回る時の設定(by t:1109)
-    if @kk_h >= 24
-      @kk_h -= 24
+    if @skk_h >= 24
+      @skk_h -= 24
     end
 
-     # 0-9分以内の場合の分表示の設定(by t:1109)
-    if @kk_m < 10
-      @kk_m = "0" + @kk_m.to_s
+
+
+    # 差分の目安の時間、分の変数を用意(不変)
+    @dif_hour = @hour.to_i - @c_hour.to_i
+    @dif_minute = @minute.to_i -@c_minute.to_i
+
+
+    # 中継時間を設定した場所が近い場所なら
+    @sd1hour = 0
+    @sd1minute = 0
+    @sd2time = @sk_res[1]['rows'][0]['elements'][0]['duration']['text']
+    @sd2hour = 0
+    @sd2minute = 0
+
+
+    if destination_1 == @near1
+      @sd1hour = @sk_hour - @hour.to_i
+      @sd1minute = @sk_minute - @minute.to_i
+
+      if @sd2time =~ /\shours|\shour/
+        @sd2hour = $`.to_i
+        if @sd2time =~ /\shours\s(.+)\smins|\shours\s(.+)\smin|\shour\s(.+)\smins|\shour\s(.+)\smin/
+          @sd2minute = $+.to_i
+        end
+      elsif @sd2time =~ /\smins|\smin/
+        @sd2minute = $`.to_i
+      end
+
+      if @kk_time =~ /\shours|\shour/
+        @sd2hour = @sd2hour + $`.to_i
+        if @kk_time =~ /\shours\s(.+)\smins|\shours\s(.+)\smin|\shour\s(.+)\smins|\shour\s(.+)\smin/
+          @sd2minute = @sd2minute + $+.to_i
+        end
+      elsif @kk_time =~ /\smins|\smin/
+        @sd2minute = $`.to_i + @sd2minute
+      end 
+
+    elsif destination_1 == @near2
+      @sd1hour = @skk_h - @hour.to_i
+      @sd1minute = @skk_m - @minute.to_i
+
+      if @sd2time =~ /\shours|\shour/
+        @sd2hour = $`.to_i
+        if @sd2time =~ /\shours\s(.+)\smins|\shours\s(.+)\smin|\shour\s(.+)\smins|\shour\s(.+)\smin/
+          @sd2minute = $+.to_i
+        end
+      elsif @sd2time =~ /\smins|\smin/
+        @sd2minute = $`.to_i
+      end
     end
-    
+
+   #  時間の比較のための変数
+   @sd1hour = @sd1hour.to_s
+   @sd1minute = @sd1minute.to_s
+   @sd2hour = @sd2hour.to_s
+   @sd2minute = @sd2minute.to_s
+   @dif_hour = @dif_hour.to_s
+   @dif_minute = @dif_minute.to_s
+
+
+   @ordertime = @sd1hour + @sd1minute
+   @ordertime = @ordertime.to_i
+   @reversetime = @sd2hour + @sd2minute
+   @reversetime = @reversetime.to_i 
+   @diftime = @dif_hour + @dif_minute
+   @diftime = @diftime.to_i
+
+   orderz = @ordertime - @diftime
+   reversez = @reversetime - @diftime
+   
+
+   @handan = ""
+   if orderz.abs <= reversez.abs
+     @handan = "そのままのルートで適切である"
+   else
+     @handan = "逆順で行った方が適切である"
+   end
+   
+
+
+
+
+
+
+
   end
 end
     
