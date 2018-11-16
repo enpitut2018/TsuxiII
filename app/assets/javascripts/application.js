f93e031d35c8e290557bef756ba85cee0015efbc
IkumaOka <ikuma1018soccer06@gmail.com>
2018-11-16 15:59:04 +0900

    add field and refactor code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,48 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+
+$(function(){
+    var idNo = 1;
+    
+    // 追加ボタン押下時イベント
+    $('button#addButton').on('click',function(){
+        $('div#templateForm')
+            // コピー処理
+            .clone(true)
+            // 不要なID削除
+            .removeAttr("id")
+            // 非表示解除
+            .removeClass("notDisp")
+            // テキストボックスのID追加
+            .find("input[name=templateTextbox]")
+            .attr("id", "textbox_" + idNo)
+            .end()
+            // ボタンのID追加
+            .find("button[name=templateButton]")
+            .on('click',function(){alert($(this).attr('id'));})
+            .attr("id", "button_" + idNo)
+            .end()
+            // 情報表示
+            .find("span.dispInfo")
+            .text("id[" + idNo + "] TextBox_ID[" + "textbox_" + idNo + "] Button_ID:[" + "button_" + idNo + "]")
+            .end()
+            // 追加処理
+            .appendTo("div#displayArea");
+  
+        // ID番号加算
+        idNo++;
+    });
+  
+    // 削除ボタン押下時イベント
+    $('button[name=removeButton]').on('click',function(){
+        $(this).parent('div').remove();
+    });
+    
+    // 削除ボタン押下時イベント
+    $('button[name=removeButton]').on('click',function(){
+        $(this).parent('div').remove();
+    });
+});
