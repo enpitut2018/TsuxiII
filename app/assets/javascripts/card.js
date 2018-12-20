ad83eb1180a5dbdf6679528558f61958ef87cdc6
IkumaOka <ikuma1018soccer06@gmail.com>
2018-12-20 23:43:36 +0900

    add card.js

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -5,10 +5,10 @@ $(document).on("click", ".add", function () {
 // －ボタン(class="del")がクリックされたら
 $(document).on("click", ".del", function () {
   // 2ヵ所で使うので選択された親要素を変数targetに格納 ※親要素は<div id="item">になる
-  var target = $(this).prev();
+  var target = $(this).parent();
   // targetの親要素の配下の要素数が1以下だったら ※targetの親要素は<div id="list">になる
-  // if(target.parent().children().length > 1){
+  if(target.parent().children().length > 1){
     // <div id="item">を削除する
-  target.remove();
-  // }
+    target.remove();
+  }
 });
